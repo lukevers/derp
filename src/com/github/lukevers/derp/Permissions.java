c089fdc50f251dc44706c232c89b0df18e7117dc
Luke Evers <lukevers@gmail.com>
2012-03-23 13:27:04 -0400

    fixed a few errors

--- src/com/github/lukevers/derp/Permissions.java
+++ src/com/github/lukevers/derp/Permissions.java
@@ -5,7 +5,7 @@ import org.bukkit.permissions.PermissionDefault;
 
 public class Permissions {
 	
-	public Permission derp_add = new Permission("derp.add","Bans a certain color from chatrooms",PermissionDefault.OP);
+	public Permission derp_add = new Permission("derp.add","Adds a derp to the list of derps",PermissionDefault.OP);
 	public Permission derp_reload = new Permission("derp.reload","Reloads the derp messages",PermissionDefault.OP);
 	
 	public Permissions() {
