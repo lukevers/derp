07c57993f9a195435128f6049fdf50c6e3ec4d38
Luke Evers <me@lukevers.com>
2013-12-03 13:19:38 -0500

    Start restructure

--- src/com/github/lukevers/derp/Derp.java
+++ src/com/github/lukevers/derp/Derp.java
@@ -8,25 +8,25 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Derp extends JavaPlugin {
-	
-	Logger log = Logger.getLogger("Minecraft");
-	ArrayList <String> doublederps = new ArrayList <String>();
-	
-	@Override
-	public void onDisable() {
-		log.info("CR disabled!");
-	}//close onDisable
-
-	@Override
-	public void onEnable() {
-		log.info("CR enabled!");
-		new CheckFile(doublederps);
-	}//close onEnable
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		new Commands(sender, cmd.getName(), sender.getName(), args, doublederps);
-		return true;
-	}
-	
-}//close Derp
+    
+    Logger log = Logger.getLogger("Minecraft");
+    ArrayList <String> list = new ArrayList <String>();
+    
+    @Override
+    public void onDisable() {
+	log.info("Derp commands disabled!");
+    }
+    
+    @Override
+    public void onEnable() {
+	log.info("Derp commands enabled!");
+	new CheckFile(doublederps);
+    }//close onEnable
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	new Commands(sender, cmd.getName(), sender.getName(), args, list);
+	return true;
+    }
+    
+}
